e8d85c823c703bd56499c528a8c39bfc7e201622
MatteKiwi <55237242+MatteKiwi@users.noreply.github.com>
2019-11-14 13:45:04 +0100

    asdf

---
+++ NovemberProjekt/NovemberProjekt/PokemonFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace NovemberProjekt
+{
+    class PokemonFactory
+    {
+        static PokemonFactory()
+        {
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
+            RestRequest request = new RestRequest("pokemon/804");
+            IRestResponse response = client.Get(request);
+            Pokemon p = JsonConvert.DeserializeObject<Pokemon>(response.Content);
+            //Console.WriteLine(p.types[0].type.name);
+            //Console.WriteLine(response.Content);
+            Console.WriteLine(p.name + " " + p.weight + " " + p.base_experience);
+            Console.WriteLine(p.Types);
+            // p.PrintTypes();
+        }
+
+    }
+}
--- NovemberProjekt/NovemberProjekt/Program.cs
+++ NovemberProjekt/NovemberProjekt/Program.cs
@@ -12,19 +12,9 @@ namespace NovemberProjekt
     {
         static void Main(string[] args)
         {
-            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
-            RestRequest request = new RestRequest("pokemon/804");
-            IRestResponse response = client.Get(request);
-            Pokemon p = JsonConvert.DeserializeObject<Pokemon>(response.Content);
-            //Console.WriteLine(p.types[0].type.name);
-            //Console.WriteLine(response.Content);
-            Console.WriteLine(p.name + " " + p.weight + " " + p.base_experience);
-            Console.WriteLine(p.Types);
-            // p.PrintTypes();
-
-            
-
-            Console.WriteLine(Welcome());
+            string playerName = Welcome();
+            Console.Clear();
+            Console.WriteLine(playerName + "! Welcome" );
 
             Console.ReadLine();
         }
@@ -37,13 +27,10 @@ namespace NovemberProjekt
         //En metod som tar string input ifrån klassen utils och returnar namnet på spelaren
         static string Welcome()
         {
-            Console.WriteLine("Hello and welcome to Pokemon fight sim! \n Please type in a username");
+            Console.WriteLine("Hello and welcome to Pokemon fight sim!\nPlease type in a username");
             string name = Utils.input();
-            if(Utils.TryParse.name = true)
-            {
-                return name;
-            }
-         
+            return name;
+        
         }
     }
 }
